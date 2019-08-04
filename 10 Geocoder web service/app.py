5944e1d9f3c9349cdd1a8c7100173746cc8242e9
Bharath <bharathsp14@gmail.com>
2019-08-05 00:26:36 +0530

    My first commit, Inline

---
+++ 10 Geocoder web service/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, send_file
+from geopy.geocoders import ArcGIS
+import pandas
+import datetime
+
+app=Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route('/success-table', methods=['POST'])
+def success_table():
+    global filename
+    if request.method=="POST":
+        file=request.files['file']
+        try:
+            df=pandas.read_csv(file)
+            gc=ArcGIS(scheme='http')
+            df["coordinates"]=df["Address"].apply(gc.geocode)
+            df['Latitude'] = df['coordinates'].apply(lambda x: x.latitude if x != None else None)
+            df['Longitude'] = df['coordinates'].apply(lambda x: x.longitude if x != None else None)
+            df=df.drop("coordinates",1)
+            filename=datetime.datetime.now().strftime("sample_files/%Y-%m-%d-%H-%M-%S-%f"+".csv")
+            df.to_csv(filename,index=None)
+            return render_template("index.html", text=df.to_html(), btn='download.html')
+        except Exception as e:
+            return render_template("index.html", text=str(e))
+
+@app.route("/download-file/")
+def download():
+    return send_file(filename, attachment_filename='yourfile.csv', as_attachment=True)
+
+if __name__=="__main__":
+    app.run(debug=True)
