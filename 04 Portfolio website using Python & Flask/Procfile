215d8285e3e024c7fd5e20f0c45d44a39cd598c2
JayabharathP <JayabharathP@users.noreply.github.com>
2019-08-04 23:08:38 +0530

    Add files via upload

---
+++ 04 Portfolio website using Python & Flask/Procfile
@@ -0,0 +1,1 @@
+web: gunicorn script1:app
---
+++ 04 Portfolio website using Python & Flask/script1.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/about/')
+def about():
+    return render_template("about.html")
+
+if __name__=="__main__":
+    app.run(debug=True)
